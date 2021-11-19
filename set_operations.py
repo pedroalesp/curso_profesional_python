2c8101d93fd7fa576544babeecf02c5f23ea85b7
Pedro Silva <pedroalejsp@gmail.com>
2021-11-19 19:19:51 -0300

    Sets operations

---
+++ set_operations.py
@@ -0,0 +1,9 @@
+def run():
+    my_set = {1, 2, 3, 4}
+    my_another_set = {1, 2, 5, 6, 7}
+    final_set =  my_another_set - my_set
+    print(final_set)
+
+
+if __name__ == '__main__':
+    run()
